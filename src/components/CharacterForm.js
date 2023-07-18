483d5b4ebb863e6eca6604fbd3992bb28bafd73c
Shania Lopez <slopez@fandm.edu>
2023-07-17 21:24:44 -0400

    Basic form inputs created

--- src/components/CharacterForm.js
+++ src/components/CharacterForm.js
@@ -4,7 +4,15 @@ function CharacterForm() {
     return (
         <form>
             <label>Create A Character</label><br />
-            <input type="text" name="name" placeholder="Name..."></input>
+            <input type="text" name="name" placeholder="Name..."></input><br />
+            <input type="number" name="hp" placeholder="HP..."></input><br />
+            <select>
+                <option>Archetype</option>
+                <option>Healer</option>
+                <option>Ranger</option>
+                <option>Fighter</option>
+                <option>Barbarian</option>
+            </select><br />
             <button type="submit">Submit</button>
         </form>
     )
