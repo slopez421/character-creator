885f3168ab60620af7aea977ae050887187f9767
Shania Lopez <slopez@fandm.edu>
2023-07-19 19:52:03 -0400

    changed the card layout

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-function Card() {
-    return <p>hi</p>
+function Card({character}) {
+    const {name, race, archetype, hp} = character;
+
+    return <div className="card">
+        <h4>Name: {name}<br />
+        Race: {race}<br />
+        Archetype: {archetype}<br />
+        HP: {hp}</h4>
+    </div>
 }
 
 export default Card;
--- src/components/CharacterContainer.js
+++ src/components/CharacterContainer.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Card from "./Card";
 
 function CharacterContainer({characters}) {
 
 return <div className="container">
     <h2>Character Container</h2>
     {characters.map((character) => {
-        return <h2>{character.name}</h2>
+        return <Card key={character.id} character={character} />
     })}
 
 </div>
