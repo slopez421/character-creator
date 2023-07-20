986c11e3a8d5a1a020ccc17dce534ce79a889328
Shania Lopez <slopez@fandm.edu>
2023-07-19 20:11:02 -0400

    initial formData setting state

--- src/components/CharacterForm.js
+++ src/components/CharacterForm.js
@@ -1,24 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 
 function CharacterForm() {
+const [formData, setFormData] = useState({
+    name: "",
+    hp: 0,
+    race: "",
+    archetype: ""
+})
+
+function handleChange(event) {
+   setFormData({[event.target.name]:event.target.value})
+}
+
+function handleSubmit(event) {
+event.preventDefault();
+console.log(formData)
+}
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>Create A Character</label><br />
-            <input type="text" name="name" placeholder="Name..."></input><br />
-            <input type="number" name="hp" placeholder="HP..."></input><br />
-            <select>
-                <option>Archetype</option>
-                <option>Healer</option>
-                <option>Ranger</option>
-                <option>Fighter</option>
-                <option>Barbarian</option>
+            <input type="text" name="name" value={formData.name} placeholder="Name..." onChange={handleChange}></input><br />
+            <input type="number" name="hp" value={formData.hp} placeholder="HP..." onChange={handleChange}></input><br />
+            <select  onChange={handleChange}>
+                <option value={formData.archetype}>Archetype</option>
+                <option value={formData.archetype}>Healer</option>
+                <option value={formData.archetype}>Ranger</option>
+                <option value={formData.archetype}>Fighter</option>
+                <option value={formData.archetype}>Barbarian</option>
             </select><br />
-            <select>
-                <option>Race</option>
-                <option>Goblin</option>
-                <option>Elf</option>
-                <option>Tiefling</option>
-                <option>Orc</option>
+            <select  onChange={handleChange}>
+                <option value={formData.race}>Race</option>
+                <option value={formData.race}>Goblin</option>
+                <option value={formData.race}>Elf</option>
+                <option value={formData.race}>Tiefling</option>
+                <option value={formData.race}>Orc</option>
             </select><br />
             <button type="submit">Submit</button>
         </form>
